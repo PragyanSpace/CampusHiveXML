2a424855a1b43ba5a793fc19c43c06b89167eeff
PragyanSpace <progyan.rdmi4@gmail.com>
2024-01-05 02:00:30 +0530

    security fix

--- app/src/main/java/com/example/campushive/util/AppConst.kt
+++ app/src/main/java/com/example/campushive/util/AppConst.kt
@@ -2,5 +2,4 @@ package com.example.campushive.util
 
  object AppConst {
      val dbUrl: String ="https://campushive-c4e89-default-rtdb.asia-southeast1.firebasedatabase.app"
-     val apiKey:String="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"//chat gpt api key
  }
